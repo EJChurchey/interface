120bdc2fdbf69cba6785ee24c500f4ce4ba1ce41
Your Name <everettchurchey@crbc.edu>
2015-04-07 22:27:23 -0400

    interface

--- interface/main.m
+++ interface/main.m
@@ -5,13 +5,62 @@
 //  Created by Ej Churchey on 4/7/15.
 //  Copyright (c) 2015 Ej Churchey. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+@interface car: NSObject {
+
+    int Miles;
+    int TopSpeed;
     }
+
+-(void) print;
+-(void) setMile: (int) m;
+-(void) setTopSpeed: (int) s;
+
+@end
+
+    @implementation car
+
+
+-(void) print{
+
+    NSLog (@"My car has %i Miles and a Top Speed of %i",Miles,TopSpeed);
+    
+}
+
+-(void) setMile: (int) m{
+
+    Miles = m;
+
+}
+
+-(void) setTopSpeed:(int)s{
+
+    TopSpeed = s;
+    
+}
+
+@end
+
+int main (int argc, char * argv[]) {
+    
+    @autoreleasepool {
+        
+        car *nissan;
+        
+        nissan = [car alloc];
+        nissan = [nissan init];
+        
+        [nissan setMile: 500];
+        
+        [nissan setTopSpeed: 180];
+        
+        [nissan print];
+        
+        
+    }
+    
+    
     return 0;
+    
 }
